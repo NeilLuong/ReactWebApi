8e4bc259252e45360a05eaabbbf51ff6de696517
Neil <neilath1834@gmail.com>
2024-08-01 23:28:57 -0400

    Added routing

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App/layout/App.tsx'
 import 'semantic-ui-css/semantic.min.css'
 import './App/layout/styles.css'
 import { StoreContext, store } from './App/stores/store.ts'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './App/router/Routes.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StoreContext.Provider value={store}>
-      <App />
+      <RouterProvider router={router}/>
     </StoreContext.Provider>
   </React.StrictMode>,
 )
