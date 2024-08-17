cfd44b17f8b54fa44156c3d1e43181fd1ed5c522
Neil <neilath1834@gmail.com>
2024-08-16 20:47:03 -0400

    Client side login and registration

--- client-app/src/Features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/Features/activities/dashboard/ActivityDashboard.tsx
@@ -16,7 +16,7 @@ export default observer(function ActivityDashboard() {
       if (activityRegistry.size === 0) loadActivities();
     }, [loadActivities, activityRegistry.size])
   
-    if (activityStore.loadingInitial) return <LoadingComponent content='Loading App' />
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading activities...' />
 
     return (
         <Grid>
